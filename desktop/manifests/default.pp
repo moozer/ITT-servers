9f4bd1e1c2f348bd7a0329cd54c22c6131c629a5
moz <moz@local>
2015-06-03 13:51:12 +0200

    added user using facter

--- desktop/manifests/default.pp
+++ desktop/manifests/default.pp
@@ -1,4 +1,10 @@
 # very basic installation of the xfce desktop
+user { $user:
+  ensure     => 'present',
+  password => generate('/bin/sh', '-c', "mkpasswd -m sha-512 ${password} | tr -d '\n'"),
+  managehome => true,
+}
+
 package { "task-xfce-desktop":
     ensure => "latest"
 } -> exec { "rebooting":
